4e3fe079fb3b345950cb91f48cf2ef4fd9f25839
faith muli <faithmuli33@gmail.com>
2015-04-18 12:04:41 +0300

    rails jobsite

---
+++ app/models/job.rb
@@ -0,0 +1,8 @@
+class Job < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates :company, presence: true
+	validates :url, presence: true, uniqueness: true
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+
+  resources :jobs
+  root 'jobs#index'
+
+ 
+
+end
